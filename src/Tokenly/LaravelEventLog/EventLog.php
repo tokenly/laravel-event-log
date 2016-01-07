c2665c1813400bd8815653a6c08bece0e0aec28d
Devon Weller <devon@tokenly.co>
2016-01-07 10:22:26 -0600

    added support for JSON log files

--- src/Tokenly/LaravelEventLog/EventLog.php
+++ src/Tokenly/LaravelEventLog/EventLog.php
@@ -9,20 +9,80 @@ use RuntimeException;
 
 class EventLog {
 
+    protected $write_json      = false;
+    protected $json_log_path   = null;
+    protected $json_log_stream = null;
 
-    public function __construct() {
+    protected $levels = [
+        'debug'     => 100,
+        'info'      => 200,
+        'notice'    => 250,
+        'warning'   => 300,
+        'error'     => 400,
+        'critical'  => 500,
+        'alert'     => 550,
+        'emergency' => 600,
+    ];
+
+    public function __construct($json_log_path=null) {
+        $this->write_json    = ($json_log_path !== null);
+        $this->json_log_path = $json_log_path;
     }
 
 
-    public function log($event, $raw_data, $array_keys_only=null) {
+    public function debug($event, $raw_data, $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, 'debug');
+    }
+
+    public function info($event, $raw_data, $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, 'info');
+    }
+
+    public function warning($event, $raw_data, $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, 'warning');
+    }
+
+    public function error($event, $raw_data, $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, 'error');
+    }
+
+    public function critical($event, $raw_data, $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, 'critical');
+    }
+
+    public function jsonLog($level_name, $event, $raw_data=[], $array_filter_keys=null) {
+        return $this->log($event, $raw_data, $array_filter_keys, $level_name, false, true);
+    }
+
+    public function log($event, $raw_data, $array_filter_keys=null, $level_name=null, $as_text=true, $as_json=true) {
+        if ($level_name === null) { $level_name = 'info'; }
         try {
+            $data = $this->filterLogData($raw_data, $array_filter_keys);
+
             // write to laravel log
-            Log::info($this->buildLogText($event, $raw_data, $array_keys_only));
-        } catch (RuntimeException $e) {
-            Log::error("RuntimeException in ".$e->getFile()." at line ".$e->getLine());
+            if ($as_text) {
+                Log::log($level_name, $this->buildLogText($event, $data));
+            }
+
+            // write to json log
+            if ($this->write_json AND $as_json) {
+                $this->writeToJSONLog($this->buildLogJSON($level_name, $event, $data));
+            }
+
         } catch (Exception $e) {
-            // other error
-            Log::error($e->getCode()." ".$e->getMessage()." in ".$e->getFile()." at line ".$e->getLine());
+            if ($e instanceof RuntimeException) {
+                $msg = "RuntimeException in ".$e->getFile()." at line ".$e->getLine();
+            } else {
+                $msg = "Exception (".$e->getCode().") ".$e->getMessage()." in ".$e->getFile()." at line ".$e->getLine();
+            }
+
+            // log error
+            Log::error($msg);
+
+            // write to json log
+            if ($this->write_json) {
+                $this->writeToJSONLog($this->buildLogJSON('error', 'error.log', ['msg' => $msg, 'code' => $e->getCode(), 'line' => $e->getLine(), 'file' => $e->getFile()]));
+            }
         }
     }
 
@@ -45,19 +105,60 @@ class EventLog {
         }
 
         Log::error($this->buildLogText($event, $raw_data));
+
+        // write to json log
+        if ($this->write_json) {
+            $this->writeToJSONLog($this->buildLogJSON('error', $event, $raw_data));
+        }
     }
 
-    protected function buildLogText($event, $raw_data, $array_keys_only=null) {
-        if ($array_keys_only) {
-            $data = [];
-            foreach($array_keys_only as $array_key) {
-                $data[$array_key] = $raw_data[$array_key];
-            }
-        } else {
-            $data = $raw_data;
-        }
+    // ------------------------------------------------------------------------
+
+    protected function buildLogText($event, $data) {
 
         return $event." ".str_replace('\n', "\n", json_encode($data, 192));
+    }
+
+    protected function filterLogData($raw_data, $array_filter_keys=null) {
+        if ($array_filter_keys !== null) {
+            $filtered_data = [];
+            foreach($array_filter_keys as $array_key) {
+                $filtered_data[$array_key] = $raw_data[$array_key];
+            }
+        } else {
+            if (is_array($raw_data)) {
+                $filtered_data = $raw_data;
+            } else {
+                // assume raw_data is just a string
+                $filtered_data = ['msg' => (string) $raw_data];
+            }
+        }
+
+        return $filtered_data;
+    }
+
+    protected function buildLogJSON($level_name, $event, $data) {
+        $json = array_merge([
+            'name'  => $event,
+            'ts'    => intval(microtime(true) * 1000),
+            'level' => isset($this->levels[$level_name]) ? $this->levels[$level_name] : 0,
+            'event' => $data,
+        ], $data);
+
+        return $json;
+    }
+
+    protected function writeToJSONLog($json_data) {
+        if (!is_resource($this->json_log_stream)) {
+            $this->json_log_stream = $this->openJSONLog($this->json_log_path);
+        }
+
+        fwrite($this->json_log_stream, json_encode($json_data, JSON_FORCE_OBJECT | JSON_UNESCAPED_SLASHES).PHP_EOL);
+    }
+
+    protected function openJSONLog($json_log_path) {
+        $stream = fopen($json_log_path, 'a');
+        return $stream;
     }
 
 }
--- src/Tokenly/LaravelEventLog/EventLogServiceProvider.php
+++ src/Tokenly/LaravelEventLog/EventLogServiceProvider.php
@@ -10,7 +10,12 @@ class EventLogServiceProvider extends ServiceProvider {
 
     public function register() {
         $this->app->bind('eventlog', function($app) {
-            return new EventLog();
+            $json_log_path = null;
+            if (env('USE_JSON_LOG', true)) {
+                $json_log_path = $app->storagePath().'/logs/'.env('JSON_LOG_NAME', 'log.json');
+            }
+
+            return new EventLog($json_log_path);
         });
 
     }
